23a10ab67b2b9898be26773e193731ac143558bb
Marc Tuscher <marc.tuscher@gmail.com>
2019-05-16 09:36:47 +0200

    add spinup pendulum model

--- code/mountainCar.py
+++ code/mountainCar.py
@@ -1,57 +1,197 @@
 #%%
-import itertools
+import tensorflow as tf
 import numpy as np
+import os
 import gym
-np.random.seed(0)
-env = gym.make('MountainCar-v0')
-env.seed(0)
+import time
+import sklearn
+import itertools
+import sklearn.pipeline
+import sklearn.preprocessing
+from sklearn.kernel_approximation import RBFSampler
 
 #%%
-class Agent:
-    def decide(self, observation):
-        position, velocity = observation
-        lb = min(-0.09 * (position + 0.25) ** 2 + 0.03,
-                0.3 * (position + 0.9) ** 4 - 0.008)
-        ub = -0.07 * (position + 0.38) ** 2 + 0.07
-        if lb < velocity < ub:
-            action = 2 # push right
-        else:
-            action = 0 # push left
-        return action
+env = gym.envs.make("MountainCarContinuous-v0")
+
+# Feature Preprocessing: Normalize to zero mean and unit variance
+# We use a few samples from the observation space to do this
+observation_examples = np.array([env.observation_space.sample() for x in range(10000)])
+scaler = sklearn.preprocessing.StandardScaler()
+scaler.fit(observation_examples)
+
+# Used to convert a state to a featurizes represenation.
+# We use RBF kernels with different variances to cover different parts of the space
+featurizer = sklearn.pipeline.FeatureUnion([
+    ("rbf1", RBFSampler(gamma=5.0, n_components=100)),
+    ("rbf2", RBFSampler(gamma=2.0, n_components=100)),
+    ("rbf3", RBFSampler(gamma=1.0, n_components=100)),
+    ("rbf4", RBFSampler(gamma=0.5, n_components=100))
+])
+featurizer.fit(scaler.transform(observation_examples))
 
 #%%
-agent = Agent()
-def play_once(env, agent, render=False, verbose=False):
-    traj = []
-    observation = env.reset()
-    traj += [observation]
-    episode_reward = 0.
-    for step in itertools.count():
-        if render:
+def process_state(state):
+    scaled = scaler.transform([state])
+    featurized = featurizer.transform(scaled)
+    return featurized[0]
+
+
+class PolicyNetwork:
+    def __init__(self, env, lamb=1e-5, learning_rate=0.01, scope="policy"):
+        self.env = env
+        self.lamb = lamb
+        self.learning_rate = learning_rate
+
+        with tf.variable_scope(scope):
+            self._build_model()
+            self._build_train_op()
+
+    def _build_model(self):
+        self.state = tf.placeholder(tf.float32, [400], name="state")
+
+        self.mu = tf.contrib.layers.fully_connected(
+            inputs=tf.expand_dims(self.state, 0),
+            num_outputs=1,
+            activation_fn=None,
+            weights_initializer=tf.contrib.layers.xavier_initializer()
+        )
+        self.mu = tf.squeeze(self.mu)
+
+        self.sigma = tf.contrib.layers.fully_connected(
+            inputs=tf.expand_dims(self.state, 0),
+            num_outputs=1,
+            activation_fn=None,
+            weights_initializer=tf.contrib.layers.xavier_initializer()
+        )
+        self.sigma = tf.squeeze(self.sigma)
+        self.sigma = tf.nn.softplus(self.sigma) + 1e-5
+
+        self.norm_dist = tf.contrib.distributions.Normal(self.mu, self.sigma)
+        self.action = self.norm_dist.sample(1)
+        self.action = tf.clip_by_value(self.action, self.env.action_space.low[0], self.env.action_space.high[0], name="action")
+
+    def _build_train_op(self):
+        self.action_train = tf.placeholder(tf.float32, name="action_train")
+        self.advantage_train = tf.placeholder(tf.float32, name="advantage_train")
+
+        self.loss = -tf.log(
+            self.norm_dist.prob(self.action_train) + 1e-5) * self.advantage_train - self.lamb * self.norm_dist.entropy()
+        self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+        self.train_op = self.optimizer.minimize(self.loss)
+
+    def predict(self, state, sess):
+        feed_dict = {self.state: process_state(state)}
+        return sess.run(self.action, feed_dict=feed_dict)
+
+    def update(self, state, action, advantage, sess):
+        feed_dict = {
+            self.state: process_state(state),
+            self.action_train: action,
+            self.advantage_train: advantage
+        }
+        sess.run([self.train_op], feed_dict=feed_dict)
+
+
+class ValueNetwork:
+    def __init__(self, env, learning_rate=0.01, scope="value"):
+        self.env = env
+        self.learning_rate = learning_rate
+
+        with tf.variable_scope(scope):
+            self._build_model()
+            self._build_train_op()
+
+    def _build_model(self):
+        self.state = tf.placeholder(tf.float32,[400], name="state")
+
+        self.value = tf.contrib.layers.fully_connected(
+            inputs=tf.expand_dims(self.state, 0),
+            num_outputs=1,
+            activation_fn=None,
+            weights_initializer=tf.contrib.layers.xavier_initializer()
+        )
+        self.value = tf.squeeze(self.value, name="value")
+
+    def _build_train_op(self):
+        self.target = tf.placeholder(tf.float32, name="target")
+        self.loss = tf.reduce_mean(tf.squared_difference(self.value, self.target))
+        self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+        self.train_op = self.optimizer.minimize(self.loss)
+
+    def predict(self, state, sess):
+        return sess.run(self.value, feed_dict={self.state: process_state(state)})
+
+    def update(self, state, target, sess):
+        feed_dict = {
+            self.state: process_state(state),
+            self.target: target
+        }
+        sess.run([self.train_op], feed_dict=feed_dict)
+
+
+def actor_critic(episodes=100, gamma=0.95, display=False, lamb=1e-5, policy_lr=0.001, value_lr=0.1):
+    tf.reset_default_graph()
+    policy_estimator = PolicyNetwork(env, lamb=lamb, learning_rate=policy_lr)
+    value_estimator = ValueNetwork(env, learning_rate=value_lr)
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    stats = []
+    saver = tf.train.Saver()
+    for i_episode in range(episodes):
+        state = env.reset()
+        reward_total = 0
+        for t in itertools.count():
+            action = policy_estimator.predict(state, sess)
+            next_state, reward, done, _ = env.step(action)
+            reward_total += reward
+
+            if display:
+                env.render()
+
+            target = reward + gamma * value_estimator.predict(next_state, sess)
+            td_error = target - value_estimator.predict(state, sess)
+
+            if i_episode<20:
+                policy_estimator.update(state, action, advantage=td_error, sess=sess)
+                value_estimator.update(state, target, sess=sess)
+
+            if done:
+                break
+            state = next_state
+        stats.append(reward_total)
+        if np.mean(stats[-100:]) > 90 and len(stats) >= 101:
+            print(np.mean(stats[-100:]))
+            print("Solved")
+        print("Episode: {}, reward: {}.".format(i_episode, reward_total))
+    saver.save(sess,'models/car.ckpt')
+    return np.mean(stats[-100:])
+
+#%%
+policy_lr, value_lr, lamb, gamma = [0.0001, 0.00046415888336127773, 2.782559402207126e-05, 0.98999999999999999]
+loss = actor_critic(episodes=100, gamma=gamma, display=False, lamb=lamb, policy_lr=policy_lr, value_lr=value_lr)
+
+
+#%%
+def play_episodes(n, env, sess, obs_ph, action_out):
+    for _ in range(n):
+        obs = env.reset()
+        done = False
+        while not done:
             env.render()
-        action = agent.decide(observation)
-        observation, reward, done, _ = env.step(action)
-        traj += [observation]
-        episode_reward += reward
-        if done:
-            break
-    if verbose:
-        print('get {} rewards in {} steps'.format(
-                episode_reward, step + 1))
-    return episode_reward, np.array(traj)
+            a = sess.run(action_out, {obs_ph: process_state(obs)})
+            print(a)
+            obs, r, done, _ = env.step(a)
 
 #%%
-data = []
-for _ in range(10):
-    _ , traj = play_once(env, agent)
-    data.append(traj)
+saver = tf.train.import_meta_graph('models/car.ckpt.meta')
+sess = tf.Session()
+saver.restore(sess, "models/car.ckpt")
+obs_ph = sess.graph.get_tensor_by_name('policy/state:0')
+action_out = sess.graph.get_tensor_by_name('policy/action:0')
+#%%
+play_episodes(3, env, sess, obs_ph, action_out)
 
 #%%
-data = np.array(data)
-print(data.shape)
-
-#%%
-print(data[0].shape)
-
+sess.graph.
 
 #%%
---
+++ code/pendulum.py
@@ -0,0 +1,8 @@
+#%%
+from D4PG.play import play
+import tensorflow as tf
+#%%
+tf.reset_default_graph()
+play()
+
+#%%
