d1f40294dffc24b9fa63190f1e3cd83e6d36f152
Marc Tuscher <marc@surfer-172-30-1-2-hotspot.internet-for-guests.com>
2019-05-28 09:30:17 +0200

    predict function

--- code/pendulum.py
+++ code/pendulum.py
@@ -1,12 +1,16 @@
 #%%
+%matplotlib inline
+%load_ext autoreload
+%autoreload 2
+
+
+#%%
+
 import numpy as np
 import pickle
 import pbdlib as pbd
 import matplotlib.pyplot as plt
 
-%matplotlib inline
-%load_ext autoreload
-%autoreload 2
 #%%
 with open('data/pendulum.pkl', 'rb') as f:
     data = pickle.load(f)
@@ -29,22 +33,19 @@ model = pbd.HMM(nb_states=4, nb_dim=4)
 model.init_hmm_kbins(demos)
 #%%
 model.em(demos, reg=1e-3)
+#%%
+model.sigma[0]
+#%%
+msg = model.predict(demos[0][0][0:3])
 
+print(msg)
 #%%
-gmm = pbd.GMM(nb_states=4, nb_dim=4, mu=model.mu, sigma=model.sigma, lmbda=model.lmbda, priors=model.priors)
+#gmm = pbd.GMM(nb_states=4, nb_dim=4, mu=model.mu, sigma=model.sigma, lmbda=model.lmbda, priors=model.priors)
 #%%
-gmr = pbd.GMR(gmm, use_pybdlib_format=True)
+#gmr = pbd.GMR(gmm, use_pybdlib_format=True)
 #%%
-d = demos[0][:4]
+#d = demos[0][:4]
 #%%
-d
-
-#%%
-inp, outp = d.T[0:3], d.T[3]
-#%%
-inp.shape
-
-#%%
-gmr.predict_GMM(inp.T, [0, 1, 2], [3])
-
+#a = gmr.predict_GMM(d[0][0:3], [0, 1, 2], [3], predict=True)
+#print(a)
 #%%
